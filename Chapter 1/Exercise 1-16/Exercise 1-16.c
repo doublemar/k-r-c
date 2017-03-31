f48311f4a6043f1a7bb49b498ade8392d53dcaae
doublemar <md-github-ac9edt@ancips.net>
2017-03-31 20:55:50 +0200

    Update Exercise 1-16.c

--- Chapter 1/Exercise 1-16/Exercise 1-16.c
+++ Chapter 1/Exercise 1-16/Exercise 1-16.c
@@ -7,53 +7,53 @@ void copy(char to[], char from[]);
 /* print the longest input line */
 main()
 {
-    int len;					/* current line length */
-	int leno;                   /* overflowed line length */
-	int max;					/* maximum length seen so far */
-	char line[MAXLINE];			/* current input line */
-	char lineo[MAXLINE];        /* overflowed input line */
-	char longest[MAXLINE];		/* longest line saved here */
+    int len;                /* current line length */
+    int leno;               /* overflowed line length */
+    int max;                /* maximum length seen so far */
+    char line[MAXLINE];     /* current input line */
+    char lineo[MAXLINE];    /* overflowed input line */
+    char longest[MAXLINE];  /* longest line saved here */
 
-	max = 0;
-	len = getline(line, MAXLINE);
-	while ((leno = len) > 0) {
-		if (len == MAXLINE-1 && line[MAXLINE-2] != '\n')
-			while ((leno = getline(lineo, MAXLINE)) > 0 && (len = len + leno) && lineo[leno-1] != '\n')
+    max = 0;
+    len = getline(line, MAXLINE);
+    while ((leno = len) > 0) {
+        if (len == MAXLINE-1 && line[MAXLINE-2] != '\n')
+            while ((leno = getline(lineo, MAXLINE)) > 0 && (len = len + leno) && lineo[leno-1] != '\n')
                 ;
-		if (len > max) {
-			max = len;
-			copy(longest, line);
-		}
-		if ((len = leno) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+        if ((len = leno) > 0)
             len = getline(line, MAXLINE);
-	}
-	if (max > 0) /* there was a line */
-		printf("%s", longest);
-		printf("\nMax length = %d", max); /* for checking */
-	return 0;
+    }
+    if (max > 0) /* there was a line */
+        printf("%s", longest);
+        printf("\nMax length = %d", max); /* for checking */
+    return 0;
 }
 
 /* getline: read a line into s, return length */
 int getline(char s[],int lim)
 {
-	int c, i;
+    int c, i;
 
-	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-	s[i] = '\0';
-	return i;
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
 
 /* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[])
 {
-	int i;
+    int i;
 
-	i = 0;
-	while ((to[i] = from[i]) != '\0')
-		++i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
 }
